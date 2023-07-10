2a7d79b38872e2f0036db9b358f36914a3398566
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-07-09 19:39:50 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+/*var item_camera= document.querySelector(".nav");
+
+//console.log(item_camera.children)
+
+for(let i=0; i<item_camera.children.length;i++){
+   var hijo=item_camera.children[i]
+        hijo.children[0].onclick=function(){
+
+            alert("333")
+        }
+
+}*/
+console.log("wwww")
+
+
