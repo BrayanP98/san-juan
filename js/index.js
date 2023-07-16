3205eb3a7f7be7f07a9851a6931acce0dd9caf58
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-07-16 15:30:37 -0500

    new menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var toogle_menu=document.querySelector("#toogle_menu");
-var menu=document.querySelector(".menu")
+var menu=document.querySelector("#lateral")
 toogle_menu.onclick=function(){
 menu.classList.toggle('active')
 
