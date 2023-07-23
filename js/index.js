7461abbd4a7f3b7b347dbae19c92a71b5960621e
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-07-23 14:19:33 -0500

    button lateral

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
+var toogle_menu_lateral=document.querySelector("#toogle_menu_lateral");
 var toogle_menu=document.querySelector("#toogle_menu");
+var button_lateral=document.querySelector("#button_lateral")
 var menu=document.querySelector("#lateral")
-toogle_menu.onclick=function(){
+
+
+toogle_menu.onclick= desplegar;
+toogle_menu_lateral.onclick=desplegar;
+
+function desplegar(){
+toogle_menu_lateral.classList.toggle("bx-rectangle")
+toogle_menu.classList.toggle("bx-rectangle")
+button_lateral.classList.toggle("active")
 menu.classList.toggle('active')
 
 }
