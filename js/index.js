364e22ab416001e6221d166a8594027903eaa84a
Brayan Perafan <120323027+Brayan-Perafan@users.noreply.github.com>
2023-07-25 22:13:38 -0500

    new puntero

--- js/index.js
+++ js/index.js
@@ -46,13 +46,19 @@ document.addEventListener("scroll", () => {
     function sigueme()
     {
     //Capto las coordenads del puntero.
-    var x = window.event.x;
-    var y = window.event.y;
-    console.log(x,y)
-    //Y se las coloco al div.
-    document.getElementById("bola").style.left = x + "px";
-    document.getElementById("bola").style.top = y + "px";
    
+    
+      document.getElementById("bola").style="display:flex"
+      var x = window.event.x;
+      var y = window.event.y;
+      //console.log(x,y)
+      //Y se las coloco al div.
+      document.getElementById("bola").style.left = x + "px";
+      document.getElementById("bola").style.top = y + "px";
+   
+     
+    
+ 
     }
 
 
